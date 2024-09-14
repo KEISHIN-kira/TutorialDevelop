8442c1086c4e274e3a0885e496089e33af901f3b
KEISHIN-kira <ykira@lycorp.co.jp>
2024-09-14 18:25:32 +0900

    first-try

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -28,58 +28,51 @@ public class UserController {
     /** 一覧画面を表示 */
     @GetMapping("/list")
     public String getList(Model model) {
-        // 全件検索結果をModelに登録
         model.addAttribute("userlist", service.getUserList());
-        // user/list.htmlに画面遷移
         return "user/list";
     }
 
     /** User登録画面を表示 */
     @GetMapping("/register")
     public String getRegister(@ModelAttribute User user) {
-        // User登録画面に遷移
         return "user/register";
     }
 
-    // ----- 変更ここから -----
     /** User登録処理 */
     @PostMapping("/register")
     public String postRegister(@Validated User user, BindingResult res, Model model) {
         if(res.hasErrors()) {
-            // エラーあり
             return getRegister(user);
         }
-        // User登録
         service.saveUser(user);
-        // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    // ----- 変更ここまで -----
 
     /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
-        // Modelに登録
+    	if(id != null) {
         model.addAttribute("user", service.getUser(id));
-        // User更新画面に遷移
-        return "user/update";
+    } else {
+    	model.addAttribute("user", model.getAttribute("user"));
     }
-
+    	return "user/update";
+    }
+    	
     /** User更新処理 */
     @PostMapping("/update/{id}/")
-    public String postUser(User user) {
-        // User登録
+    public String postUser(@Validated User user, BindingResult res, Model model) {
+    	if(res.hasErrors()) {
+    		return getUser(null, model);
+    	}
         service.saveUser(user);
-        // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
 
     /** User削除処理 */
     @PostMapping(path="list", params="deleteRun")
     public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
-        // Userを一括削除
         service.deleteUser(idck);
-        // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
 }
